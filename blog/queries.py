90ab708204592ee33b929be4579125608677f52d
David Currie <dcurrie27@gmail.com>
2023-11-03 23:39:20 +0000

    adds function to query by article id

--- blog/queries.py
+++ blog/queries.py
@@ -5,7 +5,16 @@ from blog.models import Article
 
 class ListArticlesQuery(BaseModel):
 
-    def execute(self)->List[Article]:
+    def execute(self) -> List[Article]:
         articles = Article.list()
 
         return articles
+    
+class GetArticleByIDQuery(BaseModel):
+
+    id: str
+
+    def execute(self) -> Article:
+        article = Article.get_by_id(self.id)
+
+        return article
